b7579f21a5d58e7367ca69a9b1b0002a14258322
Nathan <92413483+n8zone@users.noreply.github.com>
2024-09-17 13:36:41 -0400

    Improve logger and minor refactor

--- countries.py
+++ countries.py
@@ -1,66 +1,4 @@
-from time import perf_counter
-import verbose_logger
-from verbose_logger import create_logger, clear_log, print_log, view_log
-
-STDOUT = create_logger("stdout")
-MAX_LEN_DIFF = 4
-
-# Reconsider using default arg for log, I'd prefer to keep algorithm functions and logger decoupled
-# TODO: Implement proper insertion method, if one letter is missing, we should only need to make one change
-def compare_strings(s1, s2, log=STDOUT):
-    changes = 0
-    checking_string = s1
-    s1 = s1.lower()
-    s2 = s2.lower()
-
-    log(f"Testing {s1} against {s2}")
-
-#    if len(s2) > len(s1):
-#        temp = s1
-#        s1 = s2
-#        s2 = temp
-
-    for i in range(len(s1)):
-        char_to_check = s1[i]
-
-        try:
-            compared_to = s2[i]
-        except IndexError:
-            new_changes = (len(s1) - len(s2))
-            log(f"Reached the end of {s2}, adding {new_changes} new changes.")
-            changes += new_changes
-            break
-
-        if char_to_check == compared_to:
-            continue
-
-        log(f"{compared_to} is not {char_to_check}, adding one change")
-        changes += 1
-
-    return changes
-
-def get_most_similar(entry, known):
-    known_copy = known.copy()
-    filtered = list(filter(lambda s: abs(len(s) - len(entry)) < MAX_LEN_DIFF, known_copy))
-    STDOUT(f"Filtered countries from {len(known)} entries to {len(filtered)} entries\n{filtered}")
-    most_similar = ("ok", 999)
-    for string in filtered:
-        log = create_logger(string)
-        changes = compare_strings(string, entry, log)
-
-        if changes < most_similar[1]:
-            STDOUT(f"Changing most_similar from {most_similar} to {(string, changes)}.")
-            most_similar = (string, changes)
-
-        log(f"Result: {(string, changes)}")
-    return most_similar[0]
-
-
-
-
-
-def main():
-    countries = [
+COUNTRIES = [
         "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia",
         "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
         "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria",
@@ -85,10 +23,11 @@ def main():
         "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago",
         "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom",
         "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam",
-        "Yemen", "Zambia", "Zimbabwe", "Wales", "US Virgin Islands", "Korea"
+        "Yemen", "Zambia", "Zimbabwe", "Wales", "US Virgin Islands", "Korea", "United States"
     ]
 
-    test_cases = [
+
+test_cases = [
         ("slavania", "Slovenia"),
         ("Inia", "India"),
         ("pamama", "Panama"),
@@ -122,51 +61,3 @@ def main():
         ("serba", "Serbia"),
         ("saudi arabia", "Saudi Arabia")
     ]
-
-    case_number = 1
-    for case in test_cases:
-        trying = case[0]
-        start = perf_counter()
-        output = get_most_similar(trying, countries)
-        end = perf_counter()
-        time_to_execute = (end - start) * 1000
-        STDOUT(f"Time to execute: {time_to_execute}ms")
-        print(f"\n=========CASE {case_number}============")
-        print(f"Input: {trying}\nExpecting: {case[1]}\nActual: {output}")
-        print(f"=========CASE {case_number}============\n")
-
-        next_instr = ""
-
-
-        while next_instr != "n":
-
-            if next_instr == "vr":
-                cases = [case[1], output]
-                view_log(cases)
-
-            if next_instr == "s":
-                view_log(["stdout"])
-                input()
-
-            if next_instr != '' and next_instr[0].lower() == 'v':
-                cases = next_instr[2::].split(';')
-                print(cases)
-                view_log(cases)
-                print(
-                    f"Changes to {output}: {compare_strings(trying, output)}\nChanges to expected: {compare_strings(case[1], trying)}")
-                input()
-
-            next_instr = input(
-                "Enter 'vr' to view relevant logs\nEnter 'v' to view specific logs by country\nEnter 's' to view stdout\nEnter 'n' to continue to next case...")
-
-
-
-        verbose_logger.clear_log()
-        case_number += 1
-
-
-
-
-
-if __name__ == '__main__':
-    main()
--- verbose_logger.py
+++ verbose_logger.py
@@ -1,13 +1,57 @@
 verbose_log = ""
 
+log = {}
+
 def clear_log():
     global verbose_log
+    global  log
     verbose_log = ""
+    for key in log:
+        log[key] = ""
 
 
-def append_log(message):
+def create_logger(test_case):
+    test_case = test_case.lower()
+    if test_case not in log:
+        log[test_case] = ""
+
+    def append_msg(message):
+        log[test_case] += f"\n{message}"
+
+    return append_msg
+
+def view_log(cases=None):
+    if cases is None or cases == ['']:
+        cases = input("Enter the countries you would like to view separated by semi-colons (ex: cuba; us virgin islands\n")
+        cases = cases.split(';')
+
+    for c in cases:
+        c = c.strip().lower()
+        print_log(c)
+
+def print_log(test_case):
+    try:
+        delimiter_length = 40 + len(test_case)
+        header_length = (delimiter_length - len(test_case)) // 2
+        header_wing = "=" * header_length
+        header = header_wing + test_case.upper() + header_wing
+        delimiter = "=" * delimiter_length
+        print(f"\n{header}")
+        print(log[test_case])
+        print(f"\n{delimiter}")
+    except KeyError:
+        print(f"No associated log for {test_case}\n")
+
+
+
+
+# DEPRECATED
+def append_log(message, test_case):
+    test_case = test_case.lower()
     global verbose_log
+    global log
     verbose_log += f"\n{message}"
+    log[test_case] += f"\n{message}"
 
 # Current Issue:
 # I have to manually look through every log to find relevant test cases. If I'm confused why 'Inia' is more similar to 'Cuba' than 'India'...
